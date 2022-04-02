6795fb5c56a79255157e3c8bd400027be0a8f5b3
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-04-02 18:14:12 +0300

    Final v5 fix

--- src/components/second/ExtraComp.jsx
+++ src/components/second/ExtraComp.jsx
@@ -2,11 +2,14 @@ import React, { useState } from 'react';
 import './second.css'
 
 const ExtraComp = () => {
+
     const [count, setCount] = useState(0)
+
     return (
         <>
-<button className='btnCent' onClick={()=>setCount(count + 1)}>+</button>
-<span className='extra'>{count}</span>
+<div className='blackBox'>
+    <button className='btnCent' onClick={() => setCount(count + 1)}>+<br/>{count}</button>
+</div>
 </>
     );
 }
--- src/components/second/Second.jsx
+++ src/components/second/Second.jsx
@@ -6,20 +6,16 @@ function rndNum(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
-
-
 const Second = () => {
 
     const [addCube, setAddCube] = useState([])
-    const rnd = rndNum(0,1000)
 
     return (
         <main className='mainBase'>
             <div className='mainDiv'>
-            <button onClick={()=> setAddCube(add => ([...add, <div key={rnd} className='blackBox'><ExtraComp /></div>]))}>ADD</button>
+            <button onClick={() => setAddCube((val) => ([...val, <ExtraComp/>]))}>ADD</button>
             </div>
             <div>
-            {/* <button className='btnCent' onClick={()=>setCount(count + 1)}>+<br/>{count}</button> */}
             {addCube}
             </div>
         </main>
